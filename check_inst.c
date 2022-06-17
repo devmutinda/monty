30409c1672ec9c5097763769db5713b6eaff1dc8
devmutinda <davidkymutinda@gmail.com>
2022-06-17 09:33:55 +0000

    Create check_inst.c

---
+++ check_inst.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * check_instruction - checks if command is valid
+ * @s: instruction
+ * Return: function
+ */
+void (*check_instruction(char *s))(stack_t**, unsigned int)
+{
+	int i = 0;
+	instruction_t ops[] = {
+		{"push", op_push},
+		{"pall", op_pall},
+		{"pint", op_pint},/*To include pop, swap, add fcns*/
+		{NULL, NULL}
+	};
+
+	while (strcmp(ops[i].opcode, s) && ops[i].opcode != NULL)
+		i++;
+	return (ops[i].f);
+}
