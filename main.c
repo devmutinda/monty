e6a8f0d60c0e803550b80060037c1833386d679e
devmutinda <davidkymutinda@gmail.com>
2022-06-17 09:34:23 +0000

    Create main.c

---
+++ main.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * main - entry point
+ * @ac: argument count
+ * @av: argument vector
+ * Return: Always 0
+ */
+int main(int ac, char **av)
+{
+	FILE *fd;
+	size_t n = 0, line_number = 1;
+	stack_t *stack = NULL;
+	char *lineptr = NULL, **line;
+	int length;
+	void (*result)(stack_t**, unsigned int);
+
+	if (ac != 2)
+	{
+		dprintf(2, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	fd = fopen(av[1], "r");
+	if (fd == NULL)
+	{
+		dprintf(2, "Error: Can't open file %s\n", av[1]);
+		exit(EXIT_FAILURE);
+	}
+	while ((getline(&lineptr, &n, fd)) != -1)
+	{
+		length = strlen(lineptr);
+		lineptr[length - 1] = '\0';
+		line = split_line(lineptr);
+		result = check_instruction(line[0]);
+		value = line[1];
+		if (result == NULL)
+		{
+			dprintf(2, "L%ld: unknown instruction %s\n", line_number, lineptr);
+			exit(EXIT_FAILURE);
+		}
+		result(&stack, line_number);
+		line_number++;
+	}
+
+	return (0);
+}
