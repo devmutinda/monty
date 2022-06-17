cfa64f6640b87295a86cbf9fed304d3d21d3c177
devmutinda <davidkymutinda@gmail.com>
2022-06-17 14:45:46 +0000

    Added function prototypes to monty.h

--- monty.h
+++ monty.h
@@ -49,7 +49,10 @@ void op_pop(stack_t **stack, unsigned int line_number);
 void op_nop(stack_t **stack, unsigned int line_number);
 void op_swap(stack_t **stack, unsigned int line_number);
 void free_stack(stack_t *head);
-void op_swap(stack_t **stack, unsigned int line_number);
 int _atoi(char *s);
 void op_add(stack_t **stack, unsigned int line_number);
+void op_mul(stack_t **stack, unsigned int line_number);
+void op_sub(stack_t **stack, unsigned int line_number);
+void op_mod(stack_t **stack, unsigned int line_number);
+void op_div(stack_t **stack, unsigned int line_number);
 #endif
