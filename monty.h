5c13d2155d7dcd98613e5d61f3e669fba0486a6b
devmutinda <davidkymutinda@gmail.com>
2022-06-17 15:29:11 +0000

    Update monty.h

--- monty.h
+++ monty.h
@@ -56,4 +56,5 @@ void op_sub(stack_t **stack, unsigned int line_number);
 void op_mod(stack_t **stack, unsigned int line_number);
 void op_div(stack_t **stack, unsigned int line_number);
 void op_pchar(stack_t **stack, unsigned int line_number);
+void op_pstr(stack_t **stack, unsigned int line_number);
 #endif
