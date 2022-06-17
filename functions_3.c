34f698abe92387b145f590e826a3c634c6d4d500
devmutinda <davidkymutinda@gmail.com>
2022-06-17 14:46:47 +0000

    Create functions_3.c

---
+++ functions_3.c
@@ -0,0 +1,34 @@
+#include "monty.h"
+/**
+ * op_mod - multiplies the second top element of the stack with the top
+ * @stack: pointer to the head node pointer
+ * @line_number: the line number
+ * Return: void
+ */
+void op_mod(stack_t **stack, unsigned int line_number)
+{
+	stack_t *ptr = *stack;
+	int count = 0;
+
+	while (ptr)
+	{
+		ptr = ptr->next;
+		count++;
+	}
+	if (count < 2)
+	{
+		dprintf(2, "L%d: can't mul, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	ptr = *stack;
+	if (ptr->n == 0)
+	{
+		dprintf(2, "L%d: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	ptr->next->n = ptr->next->n % ptr->n;
+	*stack = ptr->next;
+	(*stack)->prev = NULL;
+	free(ptr);
+}
+
