f8bfec4a303f3165e824a5276748bfcdd019485c
devmutinda <davidkymutinda@gmail.com>
2022-06-17 10:23:10 +0000

    Added nop function declaration to monty.h

--- monty.h
+++ monty.h
@@ -46,6 +46,7 @@ void op_push(stack_t **stack, unsigned int line_number);
 void op_pall(stack_t **stack, unsigned int line_number);
 void op_pint(stack_t **stack, unsigned int line_number);
 void op_pop(stack_t **stack, unsigned int line_number);
+void op_nop(stack_t **stack, unsigned int line_number);
 void op_swap(stack_t **stack, unsigned int line_number);
 int _atoi(char *s);
 #endif
