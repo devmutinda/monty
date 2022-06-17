c0dd00786970c6c89885723d870db6d94f7162e4
devmutinda <davidkymutinda@gmail.com>
2022-06-17 10:09:30 +0000

    Update atoi.c

--- atoi.c
+++ atoi.c
@@ -14,22 +14,14 @@ int _atoi(char *s)
 	for (i = 0; i < len; i++)
 	{
 		if (s[i] == '-')
-		{
 			mult *= -1;
-		}
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			res = res * fact;
-			new = s[i] - 48;
-			res += new;
+		else if (s[i] < '0' || s[i] > '9')
+			return (-1);
 
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-			{
-				break;
-			}
-		}
+		res = res * fact;
+		new = s[i] - 48;
+		res += new;
 	}
 	res *= mult;
 	return (res);
-
 }
