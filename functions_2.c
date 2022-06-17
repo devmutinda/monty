21aff95c8245d8b5f9e8fc2c2ffa5d050f8a8e21
devmutinda <davidkymutinda@gmail.com>
2022-06-17 14:44:54 +0000

    Added functions to functions_2.c

--- functions_2.c
+++ functions_2.c
@@ -34,7 +34,7 @@ void op_swap(stack_t **stack, unsigned int line_number)
 void op_add(stack_t **stack, unsigned int line_number)
 {
 	stack_t *ptr = *stack;
-	int sum, count = 0;
+	int count = 0;
 
 	while (ptr)
 	{
@@ -47,8 +47,93 @@ void op_add(stack_t **stack, unsigned int line_number)
 		exit(EXIT_FAILURE);
 	}
 	ptr = *stack;
-	sum = ptr->n + ptr->next->n;
-	ptr->next->n = sum;
+	ptr->next->n = ptr->n + ptr->next->n;
+	*stack = ptr->next;
+	(*stack)->prev = NULL;
+	free(ptr);
+}
+/**
+ * op_sub - subtracts the top element of the stack from the second top
+ * @stack: pointer to the head node pointer
+ * @line_number: the line number
+ * Return: void
+ */
+void op_sub(stack_t **stack, unsigned int line_number)
+{
+	stack_t *ptr = *stack;
+	int count = 0;
+
+	while (ptr)
+	{
+		ptr = ptr->next;
+		count++;
+	}
+	if (count < 2)
+	{
+		dprintf(2, "L%d: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	ptr = *stack;
+	ptr->next->n = ptr->next->n - ptr->n;
+	*stack = ptr->next;
+	(*stack)->prev = NULL;
+	free(ptr);
+}
+/**
+ * op_div - divides the second top element of the stack by the top
+ * @stack: pointer to the head node pointer
+ * @line_number: the line number
+ * Return: void
+ */
+void op_div(stack_t **stack, unsigned int line_number)
+{
+	stack_t *ptr = *stack;
+	int count = 0;
+
+	while (ptr)
+	{
+		ptr = ptr->next;
+		count++;
+	}
+	if (count < 2)
+	{
+		dprintf(2, "L%d: can't div, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	ptr = *stack;
+	if (ptr->n == 0)
+	{
+		dprintf(2, "L%d: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	ptr->next->n = ptr->next->n / ptr->n;
+	*stack = ptr->next;
+	(*stack)->prev = NULL;
+	free(ptr);
+}
+/**
+ * op_mul - multiplies the second top element of the stack with the top
+ * @stack: pointer to the head node pointer
+ * @line_number: the line number
+ * Return: void
+ */
+void op_mul(stack_t **stack, unsigned int line_number)
+{
+	stack_t *ptr = *stack;
+	int count = 0;
+
+	while (ptr)
+	{
+		ptr = ptr->next;
+		count++;
+	}
+	if (count < 2)
+	{
+		dprintf(2, "L%d: can't mul, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	ptr = *stack;
+	ptr->next->n = ptr->next->n * ptr->n;
 	*stack = ptr->next;
 	(*stack)->prev = NULL;
 	free(ptr);
