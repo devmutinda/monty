5acced38cf495d4fa9269bea507145353833b06b
devmutinda <davidkymutinda@gmail.com>
2022-06-20 09:13:47 +0000

    Added queue and stack functions to functions_4.c

--- functions_4.c
+++ functions_4.c
@@ -22,3 +22,27 @@ void op_rotr(stack_t **stack, unsigned int line_number)
 		*stack = ptr;
 	}
 }
+/**
+ * op_stack - sets format of data to stack(LIFO)
+ * @stack: pointer to the head node pointer
+ * @line_number: the line number
+ * Return: void
+ */
+void op_stack(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+	status_stack = 1;
+}
+/**
+ * op_queue - sets format of data to queue(FIFO)
+ * @stack: pointer to the head node pointer
+ * @line_number: the line number
+ * Return: void
+ */
+void op_queue(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+	status_stack = 0;
+}
