704772ae7d2ab0b5ef2d6924fd61e87b56dc01a5
devmutinda <davidkymutinda@gmail.com>
2022-06-20 09:14:19 +0000

    Update functions.c

--- functions.c
+++ functions.c
@@ -7,7 +7,7 @@
  */
 void op_push(stack_t **stack, unsigned int line_number)
 {
-	stack_t *new;
+	stack_t *new, *ptr = *stack;
 	int n;
 
 	n = _atoi(value);
@@ -16,29 +16,36 @@ void op_push(stack_t **stack, unsigned int line_number)
 		dprintf(2, "L%d: stack not found\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-
 	if (value == NULL || (n == -1 && value[0] != '-'))
 	{
 		fprintf(stderr, "L%d: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-
 	new = malloc(sizeof(stack_t));
-
 	if (new == NULL)
 	{
 		dprintf(2, "Error: malloc failed\n");
 		free(*stack);
 		exit(EXIT_FAILURE);
 	}
-
-	new->next = *stack;
-	new->prev = NULL;
 	new->n = n;
-
-	if (*stack)
+	new->next = NULL;
+	if (!(*stack))
+		*stack = new;
+	else if (status_stack)
+	{
+		new->next = *stack;
+		new->prev = NULL;
 		(*stack)->prev = new;
-	*stack = new;
+		*stack = new;
+	}
+	else
+	{
+		while (ptr->next)
+			ptr = ptr->next;
+		ptr->next = new;
+		new->prev = ptr;
+	}
 }
 
 /**
@@ -117,4 +124,5 @@ void op_pop(stack_t **stack, unsigned int line_number)
 		(*stack)->prev = NULL;
 		free(temp);
 	}
+
 }
